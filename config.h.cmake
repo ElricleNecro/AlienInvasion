0c0144e74bb03c23e3ef65a27897b9ac003d71a0
Guillaume Plum <guiplum@gmail.com>
2014-11-28 11:56:58 +0100

    Configuration header. Will contain common information like version number.

---
+++ config.h.cmake
@@ -0,0 +1,23 @@
+#ifndef DEFINE_H
+#define DEFINE_H
+
+#ifndef NULL
+#	if defined(__GXX_EXPERIMENTAL_CXX0X__) || __cplusplus >= 201103L
+#		define NULL nullptr
+#	else
+#		define NULL 0x0
+#	endif
+#endif
+
+#define PLUGIN_API_VERSION_MAJOR @PLUGIN_API_VERSION_MAJOR@
+#define PLUGIN_API_VERSION_MINOR @PLUGIN_API_VERSION_MINOR@
+
+#define AI_VERSION_MAJOR @AI_VERSION_MAJOR@
+#define AI_VERSION_MINOR @AI_VERSION_MINOR@
+
+#define INSTALL_DIR "@CMAKE_INSTALL_PREFIX@"
+
+#define F_TYPE @F_TYPE@
+
+#endif /* end of include guard */
+//vim:ft=cpp
