7b2d7e7bbf688cf9eaea254fa5cf0c2cab6571c1
Guillaume Plum <guiplum@gmail.com>
2014-12-04 15:51:20 +0100

    Ajout d'un fichier pour premake comme test.

---
+++ premake4.lua
@@ -0,0 +1,62 @@
+solution("AlienInvasion")
+	configurations({"debug", "release"})
+		buildoptions(
+			{
+				"-std=c++11",
+				"-I include/"
+			}
+		)
+
+		flags(
+			{
+				"ExtraWarnings"
+			}
+		)
+
+	configuration("debug")
+		buildoptions(
+			{
+				"-g"
+			}
+		)
+	
+	includedirs(
+		{
+			"include/"
+		}
+	)
+
+	project("AlienInvasion")
+		language("C++")
+		kind("ConsoleApp")
+
+		location("build/bin")
+
+		files(
+			{
+				"src/main.cpp"
+			}
+		)
+
+		links(
+			{
+				"profile_gen"
+			}
+		)
+		libdirs(
+			{
+				"build/lib"
+			}
+		)
+
+	project("profile_gen")
+		language("C++")
+		kind("SharedLib")
+
+		location("build/lib")
+
+		files(
+			{
+				"src/profile/*.cpp"
+			}
+		)
