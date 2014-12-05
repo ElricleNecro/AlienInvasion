093f364538da012221cd9962b2513125923e09dc
Guillaume Plum <guiplum@gmail.com>
2014-12-05 16:01:50 +0100

    Add a way to generate automatically the config.hpp.

--- premake4.lua
+++ premake4.lua
@@ -1,3 +1,45 @@
+newoption(
+	{
+		trigger="install-prefix",
+		value="DIR",
+		description="Directory used to install bin, lib and share directory.",
+	}
+)
+
+newoption(
+	{
+		trigger="type",
+		value="TYPE",
+		description="Working type for stars coordinates.",
+	}
+)
+
+if not _OPTIONS["type"] then
+	_OPTIONS["type"] = "double"
+end
+
+if not _OPTIONS["install-prefix"] then
+	_OPTIONS["install-prefix"] = os.getenv("HOME") .. "/.local/"
+end
+
+print(_OPTIONS)
+
+io.input("config.h.cmake")
+local text = io.read("*a")
+
+text = string.gsub(text, "@F_TYPE@", _OPTIONS["type"])
+text = string.gsub(text, "@CMAKE_INSTALL_PREFIX@", _OPTIONS["install-prefix"])
+
+text = string.gsub(text, "@PLUGIN_API_VERSION_MAJOR@", "0")
+text = string.gsub(text, "@PLUGIN_API_VERSION_MINOR@", "1")
+
+text = string.gsub(text, "@AI_VERSION_MAJOR@", "0")
+text = string.gsub(text, "@AI_VERSION_MINOR@", "1")
+
+io.output("include/config.hpp")
+io.write(text)
+io.close()
+
 solution("AlienInvasion")
 	configurations({"debug", "release"})
 		buildoptions(
