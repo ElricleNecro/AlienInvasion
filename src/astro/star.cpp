4db48fb667f4c00eba7b2961b17921a87d0416f3
Guillaume Plum <guiplum@gmail.com>
2014-12-05 17:08:17 +0100

    Change the presentation.

--- src/astro/star.cpp
+++ src/astro/star.cpp
@@ -2,38 +2,39 @@
 
 namespace astro
 {
-    Star::Star(void)
-    {
-    }
+	Star::Star(void)
+	{
+	}
 
-    Star::Star(math::Point<F_TYPE> position, F_TYPE mass)
-    {
-    }
+	Star::Star(math::Point<F_TYPE> position, F_TYPE mass)
+	: _position(position)
+	{
+	}
 
-    Star::~Star(void)
-    {
-    }
+	Star::~Star(void)
+	{
+	}
 
-    math::Point<F_TYPE> Star::getPosition(void) const
-    {
-        return this->_position;
-    }
-    void Star::setPosition(math::Point<F_TYPE> position)
-    {
-        this->_position = position;
-    }
-    void Star::setPosition(std::initializer_list<F_TYPE> position)
-    {
-        // copy the position of the star
-        this->_position = position;
-    }
+	math::Point<F_TYPE> Star::getPosition(void) const
+	{
+		return this->_position;
+	}
+	void Star::setPosition(math::Point<F_TYPE> position)
+	{
+		this->_position = position;
+	}
+	void Star::setPosition(std::initializer_list<F_TYPE> position)
+	{
+		// copy the position of the star
+		this->_position = position;
+	}
 
-    F_TYPE Star::getMass(void) const
-    {
-        return this->_mass;
-    }
-    void Star::setMass(F_TYPE mass)
-    {
-        this->_mass = mass;
-    }
+	F_TYPE Star::getMass(void) const
+	{
+		return this->_mass;
+	}
+	void Star::setMass(F_TYPE mass)
+	{
+		this->_mass = mass;
+	}
 } /* astro */
