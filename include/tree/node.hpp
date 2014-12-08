2c1f450f247dde14b0e487d50aafe996607b68db
Guillaume Plum <guiplum@gmail.com>
2014-12-08 18:15:15 +0100

    Try to correct and enhanced a little bit the class. Still do not compile, but cleaner.

--- include/tree/node.hpp
+++ include/tree/node.hpp
@@ -12,6 +12,9 @@
 
 namespace tree
 {
+	template<typename T>
+	std::string convert(T number);
+
 	class BuildError : public std::exception
 	{
 		public:
@@ -28,81 +31,175 @@ namespace tree
 
 	typedef void SwapFunc(math::Point<F_TYPE> *a, math::Point<F_TYPE> *b);
 
-    class Node {
-    	public:
-    		Node(I_TYPE* part,
-    			const int NbPart,
-    			const int NbMin,
-    			std::initializer_list<double> center,
-    			const double size,
-    			const unsigned int level=0,
-    			Node *parent=NULL
-                   );
-    		Node(I_TYPE* part,
-    			const int NbPart,
-    			const int NbMin,
-    			const double center[3],
-    			const double size,
-    			const unsigned int level=0,
-    			Node *parent=NULL
-    		       );
-    		Node(const Node &obj);
-    		virtual ~Node(void);
+	template<class T>
+	class Node {
+		public:
+			Node(
+				I_TYPE* part,
+				const int NbPart,
+				const int NbMin,
+				std::initializer_list<double> center,
+				const double size,
+				const unsigned int level=0,
+				Node *parent=NULL
+			);
+			Node(
+				I_TYPE* part,
+				const int NbPart,
+				const int NbMin,
+				const double center[3],
+				const double size,
+				const unsigned int level=0,
+				Node *parent=NULL
+			);
+			Node(const Node &obj);
+			virtual ~Node(void);
 
-    		bool Build(void);
+			bool Build(void);
 
-    		// Node Mover :
-    		Node* Up(void);
-    		Node* Down(void);
-    		Node* Next(void);
+			// Node Mover :
+			Node* Up(void);
+			Node* Down(void);
+			Node* Next(void);
 
-    		bool In(math::Point<F_TYPE> *part);
-    		Node* GetNodeOf(math::Point<F_TYPE> *part);
+			bool In(math::Point<F_TYPE>& part);
+			Node* GetNodeOf(math::Point<F_TYPE>& part);
 
-    		// Some setter and getter :
-    		int    GetLevelMax(void) const;
-    		void   SetLevelMax(int newlevel);
+			// Some setter and getter :
+			int    GetLevelMax(void) const;
+			void   SetLevelMax(int newlevel);
 
-    		int SetPart(void);
-            // io::types::Particules GetPart(void);
+			int SetPart(T& part)
+			{
+				int used = 0;
 
-    		void SetSwapFunction(SwapFunc *func);
+				for(int i=0; i<this->N; i++)
+				{
+					if( this->In(part[ this->first[i] ]) )
+					{
+						if( i != used )
+						{
+							// io::types::Swap( &this->first[i], &this->first[used]);
+						}
+						used++;
+					}
+				}
 
-    		const double* GetCenter(void) const;
+				return used;
+			};
 
-    		int GetN(void) const;
-    		int GetLevel(void) const;
-    		double GetSize(void) const;
+			const double* GetCenter(void) const;
 
-    		const double* GetMassCenter(void) const;
-    		double GetTotalMass(void) const;
+			int GetN(void) const;
+			int GetLevel(void) const;
+			double GetSize(void) const;
 
-    		std::string GetError(void);
+			const double* GetMassCenter(void) const;
+			double GetTotalMass(void) const;
 
-    		int GetDeep(void) const;
+			std::string GetError(void);
 
-    	private:
-    		Node(const Node &obj, Node *parent);
+			int GetDeep(void) const;
 
-    		unsigned int level;		/*< Niveau du noeud dans l'arbre.*/
-    		double Pos[3];			/*< Coordonnées du centre géometrique du carré.*/
-    		double cote;			/*< Longueur du côté du carré.*/
-    		void build_level(void);
-    		std::string error = "";
+		private:
+			Node(const Node &obj, Node *parent);
 
-    	protected:
-    		Node *frere,
-    			*fils,
-    			*parent;
-            I_TYPE *first;	/*< Pointeur vers la particule du tableau de particule.*/
-    		double cm[3];			/*< Position du centre de masse de la cellule.*/
-    		double M_cm;			/*< Masse totale dans la cellule.*/
-    		int N,				/*< Nombre de particule contenue dans le noeud.*/
-    		    Level_Max,
-    		    NB_Bro = 8,
-    		    Leaf;
-    		SwapFunc *Swap = NULL;
-    };
+			unsigned int level;		/*< Niveau du noeud dans l'arbre.*/
+			double Pos[3];			/*< Coordonnées du centre géometrique du carré.*/
+			double cote;			/*< Longueur du côté du carré.*/
+			void build_level(T& part)
+			{
+				int       NbUsed  = 0;
+				double    NewSide = this->cote * 0.25,
+					  x_g     = 0.,
+					  y_g     = 0.,
+					  z_g     = 0.;
+				Node **tmp     = &(this->fils);
+
+				for(int i=0; i<this->NB_Bro; i++)
+				{
+					if( (i & 1) == 0 )
+						x_g = this->Pos[0] + NewSide;
+					else
+						x_g = this->Pos[0] - NewSide;
+
+					if( (i & 2) == 0 )
+						y_g = this->Pos[1] + NewSide;
+					else
+						y_g = this->Pos[1] - NewSide;
+
+					if( (i & 4) == 0 )
+						z_g = this->Pos[2] + NewSide;
+					else
+						z_g = this->Pos[2] - NewSide;
+
+					*tmp = new Node(
+							&this->first[NbUsed],
+							this->N - NbUsed,
+							this->Leaf,
+							{
+							x_g,
+							y_g,
+							z_g
+							},
+							this->cote * 0.5,
+							this->level + 1,
+							this
+					);
+
+					//int used = this->setPart(*tmp, to_build->N - NbUsed);
+					int used = (*tmp)->SetPart(part);
+					if( used != 0 )
+					{
+						//std::cout << "Son(" << this->level << ") " << i << ": " << tmp << " " << *tmp << " " << std::endl;
+						(*tmp)->N = used;
+						if( used > this->Leaf )
+						{
+							//if( !(*tmp)->build_level() )
+							(*tmp)->Build();
+						}
+						NbUsed += used;
+						tmp = &((*tmp)->frere);
+					}
+					else
+					{
+						delete *tmp;
+						*tmp = NULL;
+					}
+				}
+
+				if( NbUsed != this->N )
+				{
+					this->error = "Some Particles were not distributed at level " + convert<int>(this->level) + " : " + convert<int>(NbUsed) + ", " + convert<int>(this->N);
+					std::string except = "Some Particles were not distributed at level " + convert<int>(this->level) +
+						": " + convert<int>(NbUsed) + ", " + convert<int>(this->N) + "\n";
+					//for(int i = NbUsed; i < this->N; i++)
+					//{
+					//except = except + "Particle " + convert<int>(i) + " ";
+					//std::cerr << "\033[32m";
+					//for(int j = 0; j < 3; j++)
+					//{
+					//std::cerr << this->first[i].Pos[j] << " (" << convert<float>(this->first[i].Pos[j]) << ")\t";
+					//except = except + convert<float>(this->first[i].Pos[j]) + " ";
+					//}
+					//std::cerr << "\033[00m" << std::endl;
+					//except = except + "\n";
+					//}
+					throw BuildError(except);
+				}
+			}
+			std::string error = "";
+
+		protected:
+			Node *frere,
+			     *fils,
+			     *parent;
+			I_TYPE *first;			/*< Pointeur vers la particule du tableau de particule.*/
+			int N,				/*< Nombre de particule contenue dans le noeud.*/
+			    Level_Max,
+			    NB_Bro = 8,
+			    Leaf;
+	};
 } /* tree */
 
 #endif /* end of include guard: NODE_HPP_2OSCA1YP */
--- src/tree/node.cpp
+++ src/tree/node.cpp
@@ -3,9 +3,6 @@
 namespace tree
 {
 	template<typename T>
-	std::string convert(T number);
-
-	template<typename T>
 	std::string convert(T number)
 	{
 		std::stringstream ss;//create a stringstream
@@ -37,14 +34,17 @@ namespace tree
 		return this->msg.c_str();
 	}
 
-	Node::Node(I_TYPE *part,
-			  const int NbPart,
-			  const int NbMin,
-			  std::initializer_list<double> center,
-			  const double size,
-			  const unsigned int level,
-			  Node *parent
-			) : level(level), cote(size), frere(NULL), fils(NULL), parent(parent), first(part), M_cm(0.), N(NbPart), Leaf(NbMin)
+	template<class T>
+	Node::Node(
+		I_TYPE *part,
+		const int NbPart,
+		const int NbMin,
+		std::initializer_list<double> center,
+		const double size,
+		const unsigned int level,
+		Node<T> *parent
+	)
+	: level(level), cote(size), frere(NULL), fils(NULL), parent(parent), first(part), N(NbPart), Leaf(NbMin)
 	{
 		if( center.size() != 3 )
 			throw BuildError("Center Coordinate must be in 3D space");
@@ -55,29 +55,33 @@ namespace tree
 		}
 	}
 
-	Node::Node( I_TYPE *part,
-			  const int NbPart,
-			  const int NbMin,
-			  const double center[3],
-			  const double size,
-			  const unsigned int level,
-			  Node *parent
-			) : level(level), cote(size), frere(NULL), fils(NULL), parent(parent), first(part), M_cm(0.), N(NbPart), Leaf(NbMin)
+	template<class T>
+	Node::Node(
+		I_TYPE *part,
+		const int NbPart,
+		const int NbMin,
+		const double center[3],
+		const double size,
+		const unsigned int level,
+		Node *parent
+	)
+	: level(level), cote(size), frere(NULL), fils(NULL), parent(parent), first(part), N(NbPart), Leaf(NbMin)
 	{
 		for(auto i = 0; i < 3; i++) {
 			this->Pos[i] = center[i];
 		}
 	}
 
-	Node::Node(const Node &obj,
-			 Node *parent
-			) : level(obj.level), cote(obj.cote), frere(NULL), fils(NULL), parent(parent), first(obj.first), M_cm(obj.M_cm), N(obj.N),
-			    Level_Max(obj.Level_Max), NB_Bro(obj.NB_Bro), Leaf(obj.Leaf)
+	template<class T>
+	Node::Node(
+		const Node &obj,
+		Node *parent
+	)
+	: level(obj.level), cote(obj.cote), frere(NULL), fils(NULL), parent(parent), first(obj.first), N(obj.N), Level_Max(obj.Level_Max), NB_Bro(obj.NB_Bro), Leaf(obj.Leaf)
 	{
 		for(int i = 0; i<3; i++)
 		{
 			this->Pos[i] = obj.Pos[i];
-			this->cm[i]  = obj.cm[i];
 		}
 
 		if( obj.fils != NULL )
@@ -86,13 +90,15 @@ namespace tree
 			this->frere = new Node(*obj.frere, this);
 	}
 
-	Node::Node(const Node &obj) :  level(obj.level), cote(obj.cote), frere(NULL), fils(NULL), parent(NULL), first(obj.first), M_cm(obj.M_cm), N(obj.N),
-						Level_Max(obj.Level_Max), NB_Bro(obj.NB_Bro), Leaf(obj.Leaf)
+	template<class T>
+	Node::Node(
+		const Node &obj
+	)
+	: level(obj.level), cote(obj.cote), frere(NULL), fils(NULL), parent(NULL), first(obj.first), N(obj.N), Level_Max(obj.Level_Max), NB_Bro(obj.NB_Bro), Leaf(obj.Leaf)
 	{
 		for(int i = 0; i<3; i++)
 		{
 			this->Pos[i] = obj.Pos[i];
-			this->cm[i]  = obj.cm[i];
 		}
 
 		if( obj.fils != NULL )
@@ -101,7 +107,8 @@ namespace tree
 			this->frere = new Node(*obj.frere, this);
 	}
 
-	bool Node::In(math::Point<F_TYPE> *point)
+	template<class T>
+	bool Node::In(math::Point<F_TYPE>& point)
 	{
 		for(int i=0; i<3; i++) {
 			if( point[i] <= this->Pos[i] - this->cote * 0.5
@@ -114,6 +121,7 @@ namespace tree
 		return true;
 	}
 
+	template<class T>
 	std::string Node::GetError(void)
 	{
 		std::string tmp = this->error;
@@ -121,6 +129,7 @@ namespace tree
 		return tmp;
 	}
 
+	template<class T>
 	int Node::GetDeep(void) const
 	{
 		if( this->fils == NULL )
@@ -136,22 +145,26 @@ namespace tree
 		return lvl;
 	}
 
+	template<class T>
 	Node* Node::Next(void)
 	{
 		return this->frere;
 	}
 
+	template<class T>
 	Node* Node::Up(void)
 	{
 		return this->parent;
 	}
 
+	template<class T>
 	Node* Node::Down(void)
 	{
 		return this->fils;
 	}
 
-	Node* Node::GetNodeOf(math::Point<F_TYPE> *part)
+	template<class T>
+	Node* Node::GetNodeOf(math::Point<F_TYPE>& part)
 	{
 		if( this->In(part) )
 		{
@@ -166,41 +179,31 @@ namespace tree
 		return NULL;
 	}
 
-	// io::types::Particules Node::GetPart(void)
-	// {
-		// return io::types::Particules(this->first);
-	// }
-
-	const double* Node::GetMassCenter(void) const
-	{
-		return (const double*)this->cm;
-	}
-
-	double Node::GetTotalMass(void) const
-	{
-		return this->M_cm;
-	}
-
+	template<class T>
 	const double* Node::GetCenter(void) const
 	{
 		return (const double*)this->Pos;
 	}
 
+	template<class T>
 	int Node::GetN(void) const
 	{
 		return this->N;
 	}
 
+	template<class T>
 	int Node::GetLevel(void) const
 	{
 		return this->level;
 	}
 
+	template<class T>
 	double Node::GetSize(void) const
 	{
 		return this->cote;
 	}
 
+	template<class T>
 	bool Node::Build(void)
 	{
 		if( this->first == NULL || this->N == 0 )
@@ -218,143 +221,108 @@ namespace tree
 		return true;
 	}
 
-	void Node::build_level(void)
-	{
-		int       NbUsed  = 0;
-		double    NewSide = this->cote * 0.25,
-		          x_g     = 0.,
-		          y_g     = 0.,
-		          z_g     = 0.;
-		Node **tmp     = &(this->fils);
+	// void Node::build_level(void)
+	// {
+		// int       NbUsed  = 0;
+		// double    NewSide = this->cote * 0.25,
+			  // x_g     = 0.,
+			  // y_g     = 0.,
+			  // z_g     = 0.;
+		// Node **tmp     = &(this->fils);
 
-		for(int i=0; i<this->NB_Bro; i++)
-		{
-			if( (i & 1) == 0 )
-				x_g = this->Pos[0] + NewSide;
-			else
-				x_g = this->Pos[0] - NewSide;
+		// for(int i=0; i<this->NB_Bro; i++)
+		// {
+			// if( (i & 1) == 0 )
+				// x_g = this->Pos[0] + NewSide;
+			// else
+				// x_g = this->Pos[0] - NewSide;
 
-			if( (i & 2) == 0 )
-				y_g = this->Pos[1] + NewSide;
-			else
-				y_g = this->Pos[1] - NewSide;
+			// if( (i & 2) == 0 )
+				// y_g = this->Pos[1] + NewSide;
+			// else
+				// y_g = this->Pos[1] - NewSide;
 
-			if( (i & 4) == 0 )
-				z_g = this->Pos[2] + NewSide;
-			else
-				z_g = this->Pos[2] - NewSide;
+			// if( (i & 4) == 0 )
+				// z_g = this->Pos[2] + NewSide;
+			// else
+				// z_g = this->Pos[2] - NewSide;
 
-			*tmp = new Node(
-						&this->first[NbUsed],
-						this->N - NbUsed,
-						this->Leaf,
-						{
-							x_g,
-							y_g,
-							z_g
-						},
-						this->cote * 0.5,
-						this->level + 1,
-						this
-					  );
+			// *tmp = new Node(
+						// &this->first[NbUsed],
+						// this->N - NbUsed,
+						// this->Leaf,
+						// {
+							// x_g,
+							// y_g,
+							// z_g
+						// },
+						// this->cote * 0.5,
+						// this->level + 1,
+						// this
+					  // );
 
-			//int used = this->setPart(*tmp, to_build->N - NbUsed);
-			int used = (*tmp)->SetPart();
-			if( used != 0 )
-			{
-				//std::cout << "Son(" << this->level << ") " << i << ": " << tmp << " " << *tmp << " " << std::endl;
-				(*tmp)->N = used;
-				if( used > this->Leaf )
-				{
-					//if( !(*tmp)->build_level() )
-					(*tmp)->Build();
-				}
-				NbUsed += used;
-				tmp = &((*tmp)->frere);
-			}
-			else
-			{
-				delete *tmp;
-				*tmp = NULL;
-			}
-		}
+			// //int used = this->setPart(*tmp, to_build->N - NbUsed);
+			// int used = (*tmp)->SetPart();
+			// if( used != 0 )
+			// {
+				// //std::cout << "Son(" << this->level << ") " << i << ": " << tmp << " " << *tmp << " " << std::endl;
+				// (*tmp)->N = used;
+				// if( used > this->Leaf )
+				// {
+					// //if( !(*tmp)->build_level() )
+					// (*tmp)->Build();
+				// }
+				// NbUsed += used;
+				// tmp = &((*tmp)->frere);
+			// }
+			// else
+			// {
+				// delete *tmp;
+				// *tmp = NULL;
+			// }
+		// }
 
-		if( NbUsed != this->N )
-		{
-			this->error = "Some Particles were not distributed at level " + convert<int>(this->level) + " : " + convert<int>(NbUsed) + ", " + convert<int>(this->N);
-			std::string except = "Some Particles were not distributed at level " + convert<int>(this->level) +
-					     ": " + convert<int>(NbUsed) + ", " + convert<int>(this->N) + "\n";
-			//for(int i = NbUsed; i < this->N; i++)
-			//{
-				//except = except + "Particle " + convert<int>(i) + " ";
-				//std::cerr << "\033[32m";
-				//for(int j = 0; j < 3; j++)
-				//{
-					//std::cerr << this->first[i].Pos[j] << " (" << convert<float>(this->first[i].Pos[j]) << ")\t";
-					//except = except + convert<float>(this->first[i].Pos[j]) + " ";
-				//}
-				//std::cerr << "\033[00m" << std::endl;
-				//except = except + "\n";
-			//}
-			throw BuildError(except);
-		}
-	}
+		// if( NbUsed != this->N )
+		// {
+			// this->error = "Some Particles were not distributed at level " + convert<int>(this->level) + " : " + convert<int>(NbUsed) + ", " + convert<int>(this->N);
+			// std::string except = "Some Particles were not distributed at level " + convert<int>(this->level) +
+					     // ": " + convert<int>(NbUsed) + ", " + convert<int>(this->N) + "\n";
+			// //for(int i = NbUsed; i < this->N; i++)
+			// //{
+				// //except = except + "Particle " + convert<int>(i) + " ";
+				// //std::cerr << "\033[32m";
+				// //for(int j = 0; j < 3; j++)
+				// //{
+					// //std::cerr << this->first[i].Pos[j] << " (" << convert<float>(this->first[i].Pos[j]) << ")\t";
+					// //except = except + convert<float>(this->first[i].Pos[j]) + " ";
+				// //}
+				// //std::cerr << "\033[00m" << std::endl;
+				// //except = except + "\n";
+			// //}
+			// throw BuildError(except);
+		// }
+	// }
 
-	int Node::SetPart(void)
-	{
-		int used = 0;
+	// int Node::SetPart(void)
+	// {
+		// int used = 0;
 
-		for(int j=0; j<3; j++)
-			this->cm[j] =0.;
-		this->M_cm = 0.;
+		// for(int i=0; i<this->N; i++)
+		// {
+			// if( this->In(this->first[i]) )
+			// {
+				// if( i != used )
+				// {
+					// // io::types::Swap( &this->first[i], &this->first[used]);
+				// }
+				// used++;
+			// }
+		// }
 
-		for(int i=0; i<this->N; i++)
-		{
-			if( this->In(&this->first[i]) )
-			{
-				for(int j=0; j<3; j++)
-					this->cm[j] += this->first[i].Pos[j];
-				this->M_cm += this->first[i].m;
+		// return used;
+	// }
 
-				if( i != used )
-				{
-					// io::types::Swap( &this->first[i], &this->first[used]);
-				}
-				used++;
-			}
-		}
-
-		for(int j=0; j<3; j++)
-			this->cm[j] /= used;
-
-		return used;
-	}
-
-	void Node::SetSwapFunction(SwapFunc *func)
-	{
-		this->Swap = func;
-
-		std::cout << "in SetSwapFunction" << std::endl;
-
-		for(int i=0; i<3; i++)
-		{
-			for(int j=0; j<3; j++)
-				std::cout << this->first[i].Pos[j] << " ";
-			std::cout << std::endl;
-		}
-
-		this->Swap(&this->first[0], &this->first[1]);
-
-		std::cout << std::endl;
-		for(int i=0; i<3; i++)
-		{
-			for(int j=0; j<3; j++)
-				std::cout << this->first[i].Pos[j] << " ";
-			std::cout << std::endl;
-		}
-
-	}
-
+	template<class T>
 	Node::~Node(void)
 	{
 		if( this->frere != NULL )
